aceed585bce3c49b9c6d5cc8be10563cd6d12c8e
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-22 17:11:45 +0430

    stack init

--- examples/stack/main.go
+++ examples/stack/main.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+type Data interface {
+	int64 | float64 | string
+}
+
+type Node[T Data] struct {
+	Next  *Node[T]
+	Value T
+}
+
+func main() {
+	head := Node[int64]{
+		Next:  nil,
+		Value: 3,
+	}
+	fmt.Println(head.Value)
+}
