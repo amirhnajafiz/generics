4b2864495aa8dc5fbb68f3e94b1cd9b1e9f4d653
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-28 22:45:53 +0430

    fix: examples structures

--- examples/example/binary-tree/main.go
+++ examples/example/binary-tree/main.go
@@ -1,18 +1,15 @@
-// Implementing Binary tree with Golang generics
-// author: Amirhnajafiz
-// year: 2022
 package main
 
 import (
 	"fmt"
 )
 
-// Type parameter
+// Data : Type parameter.
 type Data interface {
 	int | int32 | int64 | float64
 }
 
-// Each tree node
+// Node : Each tree node.
 type Node[T Data] struct {
 	Parent *Node[T]
 	Left   *Node[T]
@@ -20,12 +17,12 @@ type Node[T Data] struct {
 	key    T
 }
 
-// Tree
+// Tree : Binary tree
 type Tree[T Data] struct {
 	Root *Node[T]
 }
 
-// Insert : add a value to tree
+// Insert : add a value to tree.
 func (tree *Tree[T]) Insert(value T) {
 	newNode := &Node[T]{
 		Parent: nil,
@@ -33,24 +30,31 @@ func (tree *Tree[T]) Insert(value T) {
 		Right:  nil,
 		key:    value,
 	}
+
 	if tree.Root == nil {
 		tree.Root = newNode
 	} else {
 		tmp := tree.Root
+
 		for {
 			holder := tmp
+
 			if value > tmp.key {
 				tmp = tmp.Right
+
 				if tmp == nil {
 					holder.Right = newNode
 					newNode.Parent = holder
+
 					break
 				}
 			} else {
 				tmp = tmp.Left
+
 				if tmp == nil {
 					holder.Left = newNode
 					newNode.Parent = holder
+
 					break
 				}
 			}
@@ -58,15 +62,18 @@ func (tree *Tree[T]) Insert(value T) {
 	}
 }
 
-// Search : search for value in tree
+// Search : search for value in tree.
 func (node *Node[T]) Search(key T) *Node[T] {
 	if node == nil {
 		fmt.Println("KEY DOES NOT EXIST !")
+
 		return nil
 	}
+
 	if node.key == key {
 		return node
 	}
+
 	if key > node.key {
 		return node.Right.Search(key)
 	} else {
@@ -74,69 +81,80 @@ func (node *Node[T]) Search(key T) *Node[T] {
 	}
 }
 
-// GetMax : get maximum element in tree
-func (root *Node[T]) GetMax() *Node[T] {
-	tmp := root
+// GetMax : get maximum element in tree.
+func (node *Node[T]) GetMax() *Node[T] {
+	tmp := node
+
 	for {
 		holder := tmp
 		tmp = tmp.Right
+
 		if tmp == nil {
 			return holder
 		}
 	}
 }
 
-// GetMin : get minimum element in tree
-func (root *Node[T]) GetMin() *Node[T] {
-	tmp := root
+// GetMin : get minimum element in tree.
+func (node *Node[T]) GetMin() *Node[T] {
+	tmp := node
+
 	for {
 		holder := tmp
 		tmp = tmp.Left
+
 		if tmp == nil {
 			return holder
 		}
 	}
 }
 
-// GetPredecessor : get predecessor of a node
+// GetPredecessor : get predecessor of a node.
 func (node *Node[T]) GetPredecessor() *Node[T] {
 	if node.Left != nil {
 		return node.Left.GetMax()
 	} else {
 		tmp := node
 		tmp2 := tmp.Parent
+
 		for tmp2 != nil {
 			if tmp != tmp2.Left {
 				break
 			}
+
 			tmp = tmp2
 			tmp2 = tmp2.Parent
 		}
+
 		return tmp2
 	}
 }
 
-// GetSuccessor : get successor of a node
+// GetSuccessor : get successor of a node.
 func (node *Node[T]) GetSuccessor() *Node[T] {
 	if node.Right != nil {
 		return node.Right.GetMin()
 	} else {
 		tmp := node
 		tmp2 := tmp.Parent
+
 		for tmp2 != nil {
 			if tmp != tmp2.Right {
 				break
 			}
+
 			tmp = tmp2
 			tmp2 = tmp2.Parent
 		}
+
 		return tmp2
 	}
 }
 
-// Delete : remove a node by value
+// Delete : remove a node by value.
 func (tree *Tree[T]) Delete(key T) {
 	node := tree.Root.Search(key)
+
 	node.DeleteNode()
 }
 
@@ -148,7 +166,6 @@ func (node *Node[T]) DeleteNode() bool {
 		if node.IsLeaf() { //node has no children
 			if node.IsRightChild() {
 				node.Parent.Right = nil
-
 			} else {
 				node.Parent.Left = nil
 			}
@@ -169,35 +186,44 @@ func (node *Node[T]) DeleteNode() bool {
 			}
 		} else { //node has two children
 			sucNod := node.GetSuccessor()
+
 			fmt.Println(sucNod.key)
+
 			holder := sucNod.key
+
 			sucNod.key = node.key
+
 			node.key = holder
+
 			fmt.Println(sucNod.key)
+
 			sucNod.DeleteNode()
 		}
 		return true
 	}
 }
 
-// IsLeaf : check if a nood is a leaf
+// IsLeaf : check if a node is a leaf.
 func (node *Node[T]) IsLeaf() bool {
 	if node.Right == nil && node.Left == nil {
 		return true
 	}
+
 	return false
 }
 
-// IsRightChild : check if a nood is right child of its parent
+// IsRightChild : check if a node is right child of its parent.
 func (node *Node[T]) IsRightChild() bool {
 	if node.key > node.Parent.key {
 		return true
 	}
+
 	return false
 }
 
 func main() {
 	tree := Tree[int]{}
+
 	tree.Insert(20)
 	tree.Insert(25)
 	tree.Insert(24)
--- examples/example/stack/main.go
+++ examples/example/stack/main.go
@@ -5,23 +5,23 @@ package main
 
 import "fmt"
 
-// Type values
+// Data Type values.
 type Data interface {
 	int64 | float64 | string
 }
 
-// Each stack house is a node
+// Node Each stack house is a node.
 type Node[T Data] struct {
 	Next  *Node[T]
 	Value T
 }
 
-// Our stack
+// Stack Our stack.
 type Stack[T Data] struct {
 	Head *Node[T]
 }
 
-// Push : insert data into stack
+// Push : insert data into stack.
 func (s *Stack[T]) Push(value T) {
 	s.Head = &Node[T]{
 		Next:  s.Head,
@@ -29,7 +29,7 @@ func (s *Stack[T]) Push(value T) {
 	}
 }
 
-// Pop : remove data from stack
+// Pop : remove data from stack.
 func (s *Stack[T]) Pop() *Node[T] {
 	if s.Head != nil {
 		temp := s.Head
